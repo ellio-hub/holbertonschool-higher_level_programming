4545d274e44e3c1f2f3be8c1eb8953cb6d6446ba
ali mansour <1421@holbertonschool.com>
2020-12-07 21:46:35 +0100

    1st push

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - function to check cycles in singly linked lists
+ * @list: pointer to head of list
+ * Return: bit
+ */
+
+int check_cycle(listint_t *list)
+{
+listint_t *i = list, *j = list;
+while (i && j && j->next)
+{
+i = i->next;
+j = j->next->next;
+if (i == j)
+return (1);
+}
+return (0);
+}
