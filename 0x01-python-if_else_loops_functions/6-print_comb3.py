d77c0862c87da72dd8656b69698eaad971c4a78a
ali mansour <1421@holbertonschool.com>
2020-12-08 22:29:57 +0100

    1st push

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i < j and i != 8:
-            print("{}{} ,".format(i, j), end="")
+            print("{}{}, ".format(i, j), end="")
         elif i == j - 1 and j == 9:
             print("{}{}".format(i, j))
