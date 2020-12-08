76d862a20e37a75da14a572a6d1bf83fdb4f2c79
ali mansour <1421@holbertonschool.com>
2020-12-09 00:09:29 +0100

    1st push

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,40 +9,5 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *x = NULL, *last = NULL, *new = NULL;
-x = *head;
-last = *head;
-new = malloc(sizeof(listint_t));
-if (!new)
 return (NULL);
-new->n = number;
-if (!x)
-{
-new->next = NULL;
-*head = new;
-return (*head);
-}
-else if ((*head)->n > number)
-{
-new->next = (*head);
-(*head) = new;
-return (*head);
-}
-while (x)
-{
-if (x->n > number)
-{
-new->next = x;
-last->next = new;
-break;
-}
-last = x;
-x = x->next;
-}
-if (!x)
-{
-new->next = NULL;
-last->next = new;
-}
-return (*head);
 }
