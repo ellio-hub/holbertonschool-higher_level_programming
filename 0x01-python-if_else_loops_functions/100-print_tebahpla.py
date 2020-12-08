b684dfc10dea65217435dc8f9b2bb9b84274a547
ali mansour <1421@holbertonschool.com>
2020-12-09 00:07:39 +0100

    1st push

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(122, 96, -2):
-    print("{}".format(chr(x - 32)), end="")
-    print("{}".format(chr(x - 1)), end="")
+    print("{}".format(chr(x)), end="")
+    print("{}".format(chr(x - 33)), end="")
