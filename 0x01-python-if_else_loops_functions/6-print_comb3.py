9f2a80d7a42ae5c35c190af99b2eae6ed7568082
ali mansour <1421@holbertonschool.com>
2020-12-08 22:25:44 +0100

    1st push

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i < j and i != 8:
+            print("{}{} ,".format(i, j), end="")
+        elif i == j - 1 and j == 9:
+            print("{}{}".format(i, j))
