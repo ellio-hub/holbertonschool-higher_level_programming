32971daeb6467b266ccef6957f38c025db011144
ali mansour <1421@holbertonschool.com>
2021-01-10 22:05:36 +0100

    2nd

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     try:
         a = a / b
     except ZeroDivisionError:
-        a = "none"
+        a = "None"
     finally:
         print("Inside result: {}".format(a))
     return(a)
