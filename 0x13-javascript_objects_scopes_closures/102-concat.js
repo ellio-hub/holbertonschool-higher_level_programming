7544624ce359264f963d424652cf4a688a4ac33e
ali mansour <1421@holbertonschool.com>
2021-03-24 23:10:23 +0100

    1st push

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let file;
+const fs = require('fs');
 const f1p = process.argv[2];
 const f2p = process.argv[3];
 const f3p = process.argv[4];
-const f1 = file.readFileSync(f1p);
-const f2 = file.readFileSync(f2p);
-file.writeFileSync(f3p, f1 + f2);
+const f1 = fs.readfsSync(f1p);
+const f2 = fs.readfsSync(f2p);
+fs.writefsSync(f3p, f1 + f2);
