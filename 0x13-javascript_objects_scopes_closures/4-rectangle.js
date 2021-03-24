9f6b505c56c590e7b6ffba7fb1640d1885708d0d
ali mansour <1421@holbertonschool.com>
2021-03-24 21:48:21 +0100

    1st push

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w * h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let c = '';
+    let i;
+    for (i = 0; i < this.width; i++) {
+      c += 'X';
+    }
+    for (i = 0; i < this.height; i++) {
+      console.log(c);
+    }
+  }
+
+  rotate () {
+    const x = this.width;
+    this.width = this.height;
+    this.height = x;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
